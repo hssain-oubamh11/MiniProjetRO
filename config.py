668db26398416acd733b017abfc9c7e240d26ffa
UIbrahim <mamadouibrahim12345@icloud.com>
2026-05-10 01:51:49 +0100

    ADD: batiment.py

---
+++ config.py
@@ -0,0 +1,21 @@
+# CE fichier contient toutes les constantes du projet
+from enum import Enum
+class EtatNoeud(Enum):
+    LIBRE = "libre" 
+    ENFUME = "enfume"
+    BLOQUE = "bloque"
+    EN_FEU = "en_feu"
+   
+class TypeNoeud(Enum):
+     SALLE = "salle"
+     COULOIR = "couloir"
+     ESCALIER = "escalier"
+     SORTIE = "sortie"
+
+class EtatArc(Enum):
+    LIBRE = "libre"
+    RALENTI ="ralenti"
+    BLOQUE = "bloque"
+
+SEUIL_ENFUME=0.4
+SEUIL_BLOQUE=0.8
